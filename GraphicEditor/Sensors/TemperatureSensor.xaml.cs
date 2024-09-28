feac531c5e9ad53dd7f8ea8e95b43b6ec60c0e4c
Александр Мокринский <mokrynskiy@gmail.com>
2024-09-28 11:13:26 +0700

    Добавьте файлы проекта.

---
+++ GraphicEditor/Sensors/TemperatureSensor.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace GraphicEditor.Sensors
+{
+    /// <summary>
+    /// Логика взаимодействия для TemperatureSensor.xaml
+    /// </summary>
+    public partial class TemperatureSensor : ContentControl
+    {
+        public TemperatureSensor()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnMouseDoubleClick(MouseButtonEventArgs e)
+        {
+            if(this.Parent is Canvas)
+            {
+                var canv = (Canvas)this.Parent;
+                Selector.SetIsSelected(this, true);
+            }
+           
+        }
+    }
+}
